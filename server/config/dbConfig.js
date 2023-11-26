4fa6507ad0bc76e78a53a743b521190f1cccc48f
AdityaAg10 <aditya.agarwal493@gmail.com>
2023-11-26 17:46:14 +0530

    first

---
+++ server/config/dbConfig.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb+srv://adityaagarwal:QJ0MEZuVakDRRoYl@cluster69.otm3agd.mongodb.net/Project_1", { useNewUrlParser: true, useUnifiedTopology: true });
+
+const connection = mongoose.connection;
+
+connection.on('connected', () => {
+    console.log('Mongo db connected successfully');
+});
+
+connection.on('error', (err) => {
+    console.log('Mongo db connection error: ', err);
+});
+
+module.exports = mongoose;
